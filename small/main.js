14c66c6f1b132a94772c214bb9679d04ebffb101
HeleneElena <91485528+HeleneElena@users.noreply.github.com>
2022-01-30 12:13:25 +0100

    Modal window

---
+++ small/main.js
@@ -0,0 +1,17 @@
+let btn = document.querySelector('.btn');
+let modal = document.querySelector('.modal');
+let closeModal = document.querySelector('.close_modal_window');
+
+btn.addEventListener('click', function() {
+    modal.style.display = 'block';
+});
+
+closeModal.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+});

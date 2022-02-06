9b0d9f1cbd3cb010f859eded90b44f3d14b1329b
HeleneElena <91485528+HeleneElena@users.noreply.github.com>
2022-02-06 10:19:45 +0100

    алиСайл1

--- алиСайл/main.js
+++ алиСайл/main.js
@@ -8,21 +8,34 @@ const formSearch = document.querySelector('.form-search'),
 const city = ['Moskau', 'Regensburg', 'Minich', 'Berlin', 'Leipzig', 'Dresden', 'Kemerovo', 'Novosibirsk',
                'Prag', 'Barselona'];
 
-    inputCitiesFrom.addEventListener('input', () => {
-        dropdownCitiesFrom.textContent = '';
+const showCity = (input, list) => {
+        list.textContent = '';
 
-        if (inputCitiesFrom.value !== '') {
+        if (input.value !== '') {
 
             const filterCity = city.filter((item) => {
                 const fixItem = item.toLowerCase();
-                return fixItem.includes(inputCitiesFrom.value.toLowerCase());
+                return fixItem.includes(input.value.toLowerCase());
             });
             filterCity.forEach((item) => {
                 const li = document.createElement('li');
                 li.classList.add('dropdown__city');
                 li.textContent = item;
-                dropdownCitiesFrom.append(li);
+                list.append(li);
                 console.log(li);
             });
         } 
+    };
+
+    inputCitiesFrom.addEventListener('input', () => {
+        showCity(inputCitiesFrom, dropdownCitiesFrom);
     });
+    dropdownCitiesFrom.addEventListener('click', (event) => {
+        const target = event.target;
+        if (target.tagName.toLowerCase() === 'li') {
+            inputCitiesFrom.value = target.textContent;
+            dropdownCitiesFrom.textContent = '';
+        }
+    });
+
+

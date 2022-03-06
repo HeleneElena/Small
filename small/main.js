afad8bf3c07ee4ea2320e8c7dc0f2182e2edf1a2
HeleneElena <91485528+HeleneElena@users.noreply.github.com>
2022-03-06 20:00:19 +0100

    Add files via upload

--- small/main.js
+++ small/main.js
@@ -1,75 +1,151 @@
-const formSearch = document.querySelector('.form-search'),
-      inputCitiesFrom = document.querySelector('.input__cities-from'),
-      dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
-      dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
-      inputCitiesTo = document.querySelector('.input__cities-to'),
-      inputDateDepart = document.querySelector('.input__date-depart');
+const questions = [
+    {
+        question: 'Welche Sprache funktioniert im Browser?',
+        answers: ['Java', 'C', 'Python', 'JavaScript',],
+        correct: 4,
+    },
+    {
+        question: 'Was bedeutet CSS?',
+        answers: [
+            'Central Style Sheets', 
+            'Cascading Style Sheets', 
+            'Cascading Simple Sheets', 
+            'Cars Style Sheetst',
+        ],
+        correct: 2,
+    },
+    {
+        question: 'Was bedeutet HTML?',
+        answers: [
+            'Hypertext Markup Language', 
+            'Hypertext Mardown Language', 
+            'Hypertext Machine Language', 
+            'Helicopters Terminals Motoboats Language',
+        ],
+        correct: 1,
+    },
+    {
+        question: 'Im welchem Jahr wurde das JavaScript erstellt?',
+        answers: ['1996', '1995', '1994', '2000'],
+        correct: 2,
+    },
+];
 
-const city = ['Moskau', 'Regensburg', 'Minich', 'Berlin', 'Leipzig', 'Dresden', 'Kemerovo', 'Novosibirsk',
-               'Prag', 'Barselona'];
+const quizHeader = document.querySelector('.quiz-header');
+const quizList = document.querySelector('.quiz-list');
+const btn = document.querySelector('.quiz-submit');
 
-  // 
-  const getData = () => {
-      const request = new XMLHttpRequest();   // объект запроса
+let score = 0; // кол-во правильных ответов
+let questionIndex = 0; // текущий вопрос
 
-      request.open('GET', url);
+clearPage();
+showQestion();
+btn.addEventListener('click', checkAnswer);
 
-      request.addEventListener('readystatechange', () => {
-        if (request.readyState !== 4) return;
-            console.log(request.response);
-        if (request.status === 200) {
-            
-        } else {
-            console.error(request.status);
-        }
-      });
-      request.send();
-  };
+// очистка HTML-разметки
+function clearPage() {
+    quizHeader.innerHTML = '';
+    quizList.innerHTML = '';
+} 
 
+function showQestion() {
+    const headerTemplate = `<h2 class="title">%title%</h2>`; 
+    // вопрос
+    const title = headerTemplate.replace('%title%', questions[questionIndex]['question']);
+    quizHeader.innerHTML = title;
 
-// функция поиск городов
-const showCity = (input, list) => {
-        list.textContent = '';
-
-        if (input.value !== '') {
-
-            const filterCity = city.filter((item) => {
-                const fixItem = item.toLowerCase();
-                return fixItem.includes(input.value.toLowerCase());
-            });
-            filterCity.forEach((item) => {
-                const li = document.createElement('li');
-                li.classList.add('dropdown__city');
-                li.textContent = item;
-                list.append(li);
-                console.log(li);
-            });
-        } 
-    };
-
-    const selectCity = (event, input, list) => {
-        const target = event.target;
-        if (target.tagName.toLowerCase() === 'li') {
-            input.value = target.textContent;
-            list.textContent = '';
-        }
-    };
-
-// обработчики событий
-    inputCitiesFrom.addEventListener('input', () => {
-        showCity(inputCitiesFrom, dropdownCitiesFrom);
-    });
+    // варианты ответов
+    let answerNumber = 1;
+    for ([index, item] of questions[questionIndex]['answers'].entries()) {
         
-    inputCitiesTo.addEventListener('input', () => {
-        showCity(inputCitiesTo, dropdownCitiesTo);
-    });
+        const questionTemplate = `
+            <li>
+                <label>
+                    <input value="%number%" type="radio" class="answer" name="answer" />
+                    <span>%answer%</span>
+                </label>
+            </li>
+        `;
 
-    dropdownCitiesFrom.addEventListener('click', (event) => {
-         selectCity(event, inputCitiesFrom, dropdownCitiesFrom);
-    });
+        const answerHTML = questionTemplate
+                                .replace("%answer%", item)
+                                .replace("%number%", answerNumber);
 
-    dropdownCitiesTo.addEventListener('click', (event) => {
-          selectCity(event, inputCitiesTo, dropdownCitiesTo); 
-    });
+        quizList.innerHTML += answerHTML;
+        answerNumber++;
+    }
 
-  getData();
+   
+
+}
+
+// функция проверки, правильно ли ответил пользователь
+ function checkAnswer() {
+
+    // находим выбранную радиокнопку
+    const checkedRadio = quizList.querySelector('input[type="radio"]:checked');
+    
+    if (!checkedRadio) {
+        btn.blur();
+        return;  // если ответ не выбран, ничего не делаем, выходим из функции
+    }
+
+    // получаем номер ответа, что выбрал пользователь
+    const userAnswer = parseInt(checkedRadio.value); // сразу переводим ответ в число
+
+    // если ответ верен, увеличиваем счет
+    if (userAnswer === questions[questionIndex]['correct']) {
+        score++;
+        console.log('score = ', score);
+    }
+    // проверка был ли это последний вопрос
+    if (questionIndex === questions.length - 1) {
+        console.log('Das ist die letzte Frage');
+        clearPage();
+        showResult();
+    } else {
+        questionIndex++; // увеличиваем индекс
+        clearPage();
+        showQestion();
+    }
+
+}
+// считаем результат
+function showResult() {
+        const resultTemplate = `
+            <h2 class="title">%title%</h2>
+            <h3 class="summary">%message%</h3>
+            <p class="result">%result%</p>
+        `;
+
+        let title, message;
+
+        // варианты заголовков и текста
+        if (score === questions.length) {
+            title = '!!!Glückwunsch!!!' ;
+            message = 'Sie haben alle Fragen richtig beantwortet!';
+        } else if (score * 100 / questions.length >= 50) {
+            title = '!!!Gutes Ergebnis!!!';
+            message = 'Sie haben mehr als die Hälfte der richtigen Antworten';
+        } else {
+            title = 'Versuchen Sie es noch einmal';
+            message = 'Sie haben weniger als die Hälfte der richtigen Antworten';
+        }
+    // результат
+     let result = `${score} aus ${questions.length}`;
+
+     // финанльный ответ
+     const finalMessage = resultTemplate
+                            .replace('%title%', title)
+                            .replace('%message%', message)
+                            .replace('%result%', result);
+
+    quizHeader.innerHTML = finalMessage;
+        
+    // меняем кнопку
+        btn.blur();
+        btn.innerHTML = 'Von neuem beginnen';
+        btn.addEventListener('click', () => history.go());
+    }
+
+        

a213a569b6ed206f25dd628978d9f4a116ca92ac
HeleneElena <91485528+HeleneElena@users.noreply.github.com>
2022-03-31 19:54:06 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Header from './Header/Header';
+
+function App() {
+  return (
+	  <> 
+    <Header />
+
+	<section className="main">
+		<h1 className="title">
+			Falcon 1
+		</h1>
+		<div className="video-container">
+			<video className="video" autoPlay loop muted src="./video/moon.mp4"></video>
+		</div>
+	</section>
+
+	</>
+  );
+}
+
+export default App;

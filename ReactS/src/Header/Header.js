d266e3d9f55cbd46d9a0bf7c9d23a819ece438fa
HeleneElena <91485528+HeleneElena@users.noreply.github.com>
2022-04-06 19:10:54 +0200

    Add files via upload

---
+++ ReactS/src/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+function Header () {
+    return (
+        <div className="Header">
+        <h1>Das ist mein Website</h1>
+        </div>
+    )
+}
+export default Header;
---
+++ ReactS/src/Section/Section.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from './../Main/Main';
+import Nav from './../Nav/Nav';
+
+function Section () {
+    return (
+        <div className='Section'>
+            <Main />
+            <Nav />
+        </div>
+    )
+}
+export default Section;

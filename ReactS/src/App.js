7f67136cd464b2dd4ed744b494c7dcece63bcfea
HeleneElena <91485528+HeleneElena@users.noreply.github.com>
2022-04-14 22:32:22 +0200

    Spiel auf React

--- ReactS/src/App.js
+++ ReactS/src/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import './App.css';
-import Header from './Header/Header';
-import Section from './Section/Section';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'; 
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    }
+  }
 
-function App() {
-  return (
- <BrowserRouter> 
-      <div className="App">
-        <Header />
-        <Section />
+  clickHandler = event => {
+    
+  }
+  render() {
+    return (
+      <div className='tic-tac'>
+        <div className='t-grid' onClick={this.clickHandler} data='0'> {this.state.squares[0]} </div>
+        <div className='t-grid' onClick={this.clickHandler} data='0'> {this.state.squares[1]} </div>
+        <div className='t-grid' onClick={this.clickHandler} data='0'> {this.state.squares[2]} </div>
+        <div className='t-grid' onClick={this.clickHandler} data='0'> {this.state.squares[3]} </div>
+        <div className='t-grid' onClick={this.clickHandler} data='0'> {this.state.squares[4]} </div>
+        <div className='t-grid' onClick={this.clickHandler} data='0'> {this.state.squares[5]} </div>
+        <div className='t-grid' onClick={this.clickHandler} data='0'> {this.state.squares[6]} </div>
+        <div className='t-grid' onClick={this.clickHandler} data='0'> {this.state.squares[7]} </div>
+        <div className='t-grid' onClick={this.clickHandler} data='0'> {this.state.squares[8]} </div>
       </div>
- </BrowserRouter> 
-  );
+    )
+  }
 }
 
 export default App;

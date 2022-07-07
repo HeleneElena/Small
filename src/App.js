9c60aa39b701315f4ea969b582aaf497d0e04ccb
EbertHelene <91485528+HeleneElena@users.noreply.github.com>
2022-07-07 19:11:22 +0200

    TodoReact

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="todo">
+      <div className='todo__sidebar'>
+        
+      </div>
+      <div className="todo__tasks"></div>
+    </div>
+  );
+}
+
+export default App;

8fd988d1cb2e28457449a38eefdee9e77839ae22
EbertHelene <91485528+HeleneElena@users.noreply.github.com>
2022-12-30 15:11:52 +0100

    простейшая сборка webpack
    одна точка входа = entry: {
            main: './main.js',
        },

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        main: './main.js',
+    },
+    output: {
+        filename: './js/[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        hot: true,
+        static: {
+            directory: './dist',
+            watch: true
+        }
+    }
+};
